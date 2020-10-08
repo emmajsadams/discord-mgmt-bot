8c773d217f8280d114cb3f2fb362909de17b448d
Emma Cline <7471922-EmmaJCline@users.noreply.gitlab.com>
2020-10-08 02:16:35 -0700

    add a health check

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setPresence({
+    activity: { type: 'WATCHING', name: 'the matrix' },
+    status: 'online',
+    afk: false,
+    url: 'https://emma.stebbins.dev/'
+  }).then(console.log)
+    .catch(console.error);
 });
 
 client.on('message', msg => {
@@ -12,3 +19,17 @@ client.on('message', msg => {
 });
 
 client.login(process.env.DISCORD_BOT_TOKEN);
+
+
+// DigitalOcean Health Check
+const express = require('express')
+const app = express()
+const port = 8080
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
