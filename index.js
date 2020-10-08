a7e058ba53f2b7dfabc24cf606d862218a88f611
Emma Cline <7471922-EmmaJCline@users.noreply.gitlab.com>
2020-10-08 03:50:01 -0700

    refactor to use asnyc await and background tasks

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const Discord = require('discord.js');
 const backup = require("discord-backup");
+
 backup.setStorageFolder(__dirname+"/backups/");
+const backupOptions = {
+  // TODO: Figure out how to download all messages without just spamming 9999
+  maxMessagesPerChannel: 99999999999999999999999999,
+  saveImages: "base64" //"base64" TODO switch to base64
+}
 
 const client = new Discord.Client({
   presence: {
     activity: { type: 'WATCHING', name: 'the matrix' },
     status: 'online',
     afk: false,
-    url: 'https://emma.stebbins.dev/'
   }
 });
 client.on('ready', () => {
@@ -27,21 +32,12 @@ const express = require('express')
 const app = express()
 const port = 8080
 
-// TODO: Switch to es6 await syntax
-app.get('/', (req, res) => {
-  client.guilds.fetch('761254466979889153', true, true)
-    .then(guild => {
-      console.log(guild);
-      backup.create(guild, {
-        // TODO: Figure out how to download all messages without just spamming 9999
-        maxMessagesPerChannel: 99999999999999999999999999,
-        saveImages: "url" //"base64" TODO switch to base64
-      }).then((backupData) => {
-        console.log(backupData)
-        res.send(`Backup created with id ${backupData.id}`);
-      })
-    })
-    .catch(console.error);
+app.get('/', async (req, res) => {
+  res.send({ success: true, message: "Backup started in background, check server ./backups for json file" });
+  const guild = await client.guilds.fetch('761254466979889153', true, true);
+  console.log(guild);
+  const backupData = await backup.create(guild, backupOptions);
+  console.log(backupData.id);
 })
 
 app.listen(port, () => {
