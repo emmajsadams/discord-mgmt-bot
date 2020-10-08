88c2a8baf35e38e9da8a6769d5ad6e260b422bce
Emma Cline <7471922-EmmaJCline@users.noreply.gitlab.com>
2020-10-08 03:15:22 -0700

    Setup backup api endpoint

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const backup = require("discord-backup");
+backup.setStorageFolder(__dirname+"/backups/");
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-  client.user.setPresence({
+const client = new Discord.Client({
+  presence: {
     activity: { type: 'WATCHING', name: 'the matrix' },
     status: 'online',
     afk: false,
     url: 'https://emma.stebbins.dev/'
-  }).then(console.log)
-    .catch(console.error);
+  }
+});
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
@@ -20,14 +22,26 @@ client.on('message', msg => {
 
 client.login(process.env.DISCORD_BOT_TOKEN);
 
-
 // DigitalOcean Health Check
 const express = require('express')
 const app = express()
 const port = 8080
 
+// TODO: Switch to es6 await syntax
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  client.guilds.fetch('761254466979889153', true, true)
+    .then(guild => {
+      console.log(guild);
+      backup.create(guild, {
+        // TODO: Figure out how to download all messages without just spamming 9999
+        maxMessagesPerChannel: 99999999999999999999999999,
+        saveImages: "url" //"base64" TODO switch to base64
+      }).then((backupData) => {
+        console.log(backupData)
+        res.send(`Backup created with id ${backupData.id}`);
+      })
+    })
+    .catch(console.error);
 })
 
 app.listen(port, () => {
