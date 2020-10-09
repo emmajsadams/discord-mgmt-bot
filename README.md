d5509fad79f129e6d27b68aa1e166d54edee09f5
Emma Cline <7471922-EmmaJCline@users.noreply.gitlab.com>
2020-10-08 19:59:41 -0700

    Implement the gpt2 tell me something random command

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # emmabot
+
+TODO: use https://www.npmjs.com/package/node-google-drive for Gdrive intergatrion
+TODO: use https://www.npmjs.com/package/node-cron for scheduled tasks, and probably switch to unix cron later when not lazy
--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const Discord = require('discord.js');
 const backup = require("discord-backup");
+const axios = require('axios');
+
+// TODO: add basic authentication to my gpt2 api
+const GPT2_API_URL = 'http://34.105.75.35'
+const EMMA_USER_ID = '325088597286060044'
+const EMMA_BOT_ID = '763599207294173234'
 
 backup.setStorageFolder(__dirname+"/backups/");
 const backupOptions = {
@@ -19,16 +25,58 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+// TODO: figure out why message content is inconsisetnt between desktop and mobile
+// content: '<@!763599207294173234> tell me something random',
+client.on('message', async message => {
+  const emmaMentionedUser = message.mentions.users.get(EMMA_BOT_ID);
+  const atEmmaBotTextInText = message.content.startsWith('@EmmaBot');
+  const emmaBotTextInText = message.content.startsWith('EmmaBot');
+  if (!emmaMentionedUser && !atEmmaBotTextInText && !emmaBotTextInText) {
+    return;
+  }
+
+  const emmaUserAuthor = message.author.id === EMMA_USER_ID;
+  message.react(':emmapray:761721006704164874');
+
+  if (message.content.includes('tell me something random')) {
+    const response = await axios.get(GPT2_API_URL, { params: { length: 100 }});
+    if (response.status !== 200) {
+      message.reply('Sorry please try again')
+      return
+    }
+    message.reply(response.data.text);
+
+    return;
+  }
+
+
+  if (message.content.includes('hello')) {
+    if (emmaUserAuthor) {
+      message.reply('Hello my beautiful queen!');
+    } else {
+      message.reply('Begone human filth');
+    }
+
+    return;
+  }
+
+  // figure out embed
+  // {
+  //   embed: {
+  //     url: 'https://youtu.be/ARJ8cAGm6JE?t=59'
+  //   }
+  // }
+  if (emmaUserAuthor) {
+    message.reply('My beautiful queen I do not understand!');
+  } else {
+    message.reply("I'm sorry human filth, I'm afraid I can't do that.")
   }
 });
 
 client.login(process.env.DISCORD_BOT_TOKEN);
 
 // DigitalOcean Health Check
-const express = require('express')
+const express = require('express');
 const app = express()
 const port = 8080
 
