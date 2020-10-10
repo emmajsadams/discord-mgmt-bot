dbbea707e8cff847d0175f0bc22a92e551e87d04
Emma Cline <7471922-EmmaJCline@users.noreply.gitlab.com>
2020-10-10 00:10:18 -0700

    Call setup backups in main bot

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+const setupCron = require("./src/setupCron");
 const setupEventHandlers = require("./src/setupEventHandlers");
 const setupServer = require("./src/setupServer");
 
+setupCron();
 setupEventHandlers();
 setupServer();
