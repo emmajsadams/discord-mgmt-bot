c528b7d0a34b2099728506b843e2110d8b3ee984
Emma Cline <7471922-EmmaJCline@users.noreply.gitlab.com>
2020-10-09 23:21:43 -0700

    Add full guild backup function

---
+++ src/backup.js
@@ -0,0 +1,19 @@
+const DiscordBackup = require("discord-backup");
+
+const { FRIENDS_GUILD_ID } = require('./ids');
+const DiscordClient = require('./discordClient')
+
+DiscordBackup.setStorageFolder(__dirname+"/../backups/");
+
+const backupOptions = {
+  // TODO: Figure out how to download all messages without just spamming 9999
+  maxMessagesPerChannel: 99999999999999999999999999,
+  saveImages: "base64"
+}
+
+const backup = async () => {
+  const guild = await DiscordClient.guilds.fetch(FRIENDS_GUILD_ID, true, true);
+  await DiscordBackup.create(guild, backupOptions);
+}
+
+module.exports = backup
