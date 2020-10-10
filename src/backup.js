a1d40ae80e63a1bc60a54adefd50bce9bf2249ca
Emma Cline <7471922-EmmaJCline@users.noreply.gitlab.com>
2020-10-10 00:16:22 -0700

    Use current timestamp as backup id

--- src/backup.js
+++ src/backup.js
@@ -3,17 +3,19 @@ const DiscordBackup = require("discord-backup");
 const { FRIENDS_GUILD_ID } = require("./ids");
 const DiscordClient = require("./discordClient");
 
-DiscordBackup.setStorageFolder(__dirname + "/../backups/");
-
-const backupOptions = {
-  // TODO: Figure out how to download all messages without just spamming 9999
-  maxMessagesPerChannel: 99999999999999999999999999,
-  saveImages: "base64",
-};
-
 const backup = async () => {
+  DiscordBackup.setStorageFolder(__dirname + "/../backups/");
+
   const guild = await DiscordClient.guilds.fetch(FRIENDS_GUILD_ID, true, true);
-  await DiscordBackup.create(guild, backupOptions);
+
+  const backupData = await DiscordBackup.create(guild, {
+    backupID: Date.now().toString(),
+    // TODO: Figure out how to download all messages without just spamming 9999
+    maxMessagesPerChannel: 99999999999999999999999999,
+    saveImages: "base64",
+  });
+
+  return backupData;
 };
 
 module.exports = backup;
