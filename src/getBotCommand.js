e0a022f77ad35a971d552fe795158ec02eb22e06
Emma Cline <7471922-EmmaJCline@users.noreply.gitlab.com>
2020-10-09 23:22:12 -0700

    Create function to strip botcommand from message

---
+++ src/getBotCommand.js
@@ -0,0 +1,16 @@
+const getBotCommand = messageContent => {
+  // TODO: figure out why message content is inconsisetnt between desktop and mobile
+  // content: '<@!763599207294173234> tell me something random',
+  // const emmaMentionedUser = message.mentions.users.get(EMMA_BOT_ID);
+  // const atEmmaBotTextInText = message.content.startsWith('@EmmaBot');
+  // TODO: figure out how the at mention is represented consistently across mobile, and why exclamation points dont work
+  // const EMMA_BOT_ID = '763599207294173234'
+  const emmaBotTextInText = messageContent.startsWith('EmmaBot');
+  if (!emmaBotTextInText) {
+    return null;
+  }
+
+  return messageContent.substring('EmmaBot '.length).trim();
+}
+
+module.exports = getBotCommand
