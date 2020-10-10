813a9cc92a5026c3773785eaf086fb5ed18dfbfd
Emma Cline <7471922-EmmaJCline@users.noreply.gitlab.com>
2020-10-09 23:46:16 -0700

    Port carlbot triggers over

--- src/getSimpleCommandReply.js
+++ src/getSimpleCommandReply.js
@@ -1,7 +1,8 @@
 const { EMMA_USER_ID, EVERYONE } = require("./ids");
 
+// TODO: Upload images used in these commands somewhere else rather than referencing guild specific links
 const simpleCommandResponseDict = {
-  hello: {
+  "hello": {
     [EMMA_USER_ID]: "Hello my beautiful queen!",
     [EVERYONE]: "Begone human filth",
   },
@@ -9,7 +10,20 @@ const simpleCommandResponseDict = {
     [EVERYONE]:
       "I'm afraid I can't do that https://www.youtube.com/watch?v=ARJ8cAGm6JE&t=59s",
   },
+  "fly": {
+    [EVERYONE]:
+      "https://cdn.discordapp.com/attachments/761262512485171211/763586083681599498/unknown.png",
+  },
+  "nofighting": {
+    [EVERYONE]:
+      "https://www.youtube.com/watch?v=5W75QxHgpoM",
+  },
+  "synthesis": {
+    [EVERYONE]:
+      "https://media.discordapp.net/attachments/763827152965337088/764377573366825000/unknown.png",
+  },
 };
+
 
 const getSimpleCommandReply = (authorId, command) => {
   const authorReplyDict = simpleCommandResponseDict[command];
