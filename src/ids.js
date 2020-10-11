70c20c14b41f57d5ad4db9d559129e92e246fd8f
Emma Cline <7471922-EmmaJCline@users.noreply.gitlab.com>
2020-10-10 22:22:50 -0700

    Add role menu message ids

--- src/ids.js
+++ src/ids.js
@@ -1,12 +1,17 @@
 const AWS = require("aws-sdk");
 
-// TODO: Change this file to constants.js
 module.exports = {
   EMMA_USER_ID: "325088597286060044",
   EVERYONE_USER_ID: "everyone",
   FRIENDS_GUILD_ID: "761254466979889153",
   ACQUAINTANCE_ROLE: "761255602138775572",
   EMMA_PRAY_EMOJI: ":emmapray:761721006704164874",
+  PRONOUN_MESSAGE_ID: "762153528851431445",
+  COOMER_MESSAGE_ID: "762104139739430912",
+  TABLETOP_MESSAGE_ID: "762509321694740490",
+  MINECRAFT_MESSAGE_ID: "762939489773289483",
+
+  // TODO: create a separate constants file for S3
   S3_BUCKET: "emmabot",
-  S3_API: new AWS.S3({ apiVersion: "2006-03-01", region: "us-west-2" }), // TODO: consider if this should be in a separate file with s3_Bucket for constants
+  S3_API: new AWS.S3({ apiVersion: "2006-03-01", region: "us-west-2" }),
 };
