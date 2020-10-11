4ef52d4c35e987fd0278170938eaabc64ea42c70
Emma Cline <7471922-EmmaJCline@users.noreply.gitlab.com>
2020-10-11 02:16:20 -0700

    Move index into src file for typescript change

--- index.js
+++
@@ -1,9 +0,0 @@
-const setupCron = require("./src/setupCron");
-const setupHttpRetry = require("./src/setupHttpRetry");
-const setupEventHandlers = require("./src/setupEventHandlers");
-const setupServer = require("./src/setupServer");
-
-setupHttpRetry();
-setupCron();
-setupEventHandlers();
-setupServer();
