98d4ff65ade7d37b43ff8b6dcf5cae3fa778f698
Emma Cline <7471922-EmmaJCline@users.noreply.gitlab.com>
2020-10-11 20:45:27 -0700

    Create standalone sendMessage function

---
+++ src/sendMessage.ts
@@ -0,0 +1,13 @@
+import DiscordClient from './discordClient'
+import { TextChannel, Message } from 'discord.js'
+
+export default async function sendMessage(
+  guildId: string,
+  channelId: string,
+  messageContent: string,
+): Promise<Message> {
+  const guild = await DiscordClient.guilds.fetch(guildId)
+  const channel = (await guild.channels.cache.get(channelId)) as TextChannel
+
+  return channel.send(messageContent)
+}
