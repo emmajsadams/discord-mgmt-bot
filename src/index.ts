63811b48d8a98da2abbca02c27130d3d47e08611
Emma Cline <7471922-EmmaJCline@users.noreply.gitlab.com>
2020-10-11 20:45:41 -0700

    Setup eventHandlers for logging

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+console.log('server started')
+
 export {} // TODO: figure out why this is necessary https://medium.com/@muravitskiy.mail/cannot-redeclare-block-scoped-variable-varname-how-to-fix-b1c3d9cc8206
 
 // TODO: move to types folder https://stackoverflow.com/questions/42984889/global-types-in-typescript
@@ -14,11 +16,15 @@ import { config as dotenvConfig } from 'dotenv'
 dotenvConfig()
 
 import setupCron from './setupCron'
-import setupHttpRetry from './setupHttpRetry'
 import setupEventHandlers from './setupEventHandlers'
+import setupEventHandlersForLogging from './setupEventHandlersForLogging'
+import setupHttpRetry from './setupHttpRetry'
 import setupServer from './setupServer'
 
-setupHttpRetry()
-setupCron()
-setupEventHandlers()
 setupServer()
+
+// TODO: login and only invoke these functions on ready
+setupCron()
+setupEventHandlersForLogging()
+setupEventHandlers()
+setupHttpRetry()
