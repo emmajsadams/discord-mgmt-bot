d435c4e8e3ed707f030101f1b9ad84c7391821b8
Emma Cline <7471922-EmmaJCline@users.noreply.gitlab.com>
2020-10-12 00:56:19 -0700

    Change logger to focus on important events not tracked by the audit log

--- src/setupEventHandlersForLogging.ts
+++ src/setupEventHandlersForLogging.ts
@@ -1,65 +1,22 @@
 import DiscordClient from './discordClient'
 import sendMessage from './sendMessage'
-import { FRIENDS_GUILD_ID, LOG_CHANNEL_ID } from './ids'
+import { isObjectLike, isEmpty } from 'lodash'
+import { FRIENDS_GUILD_ID, MOD_LOG_CHANNEL_ID, PUBLIC_LOG_CHANNEL_ID, ADMIN_LOG_CHANNEL_ID } from './ids'
 
-// TODO: see if api can get this list of events so it updates with api
-const eventHandlerNames = [
-  'channelCreate',
-  'channelDelete',
-  'channelPinsUpdate',
-  'channelUpdate',
-  'emojiCreate',
-  'emojiDelete',
-  'emojiUpdate',
-  'error', // TODO: figure out how to handle this
-  'guildBanAdd',
-  'guildBanRemove',
-  'guildCreate',
-  'guildDelete',
-  'guildIntegrationsUpdate',
-  'guildMemberAdd',
-  'guildMemberRemove',
-  'guildMembersChunk',
-  'guildMemberSpeaking',
-  'guildMemberUpdate',
-  'guildUnavailable',
-  'guildUpdate',
-  'invalidated', // TODO: figure out how to handle this
-  'inviteCreate',
-  'inviteDelete',
-  'messageDelete',
-  'messageDeleteBulk',
-  'messageReactionAdd',
-  'messageReactionRemove',
-  'messageReactionRemoveAll',
-  'messageReactionRemoveEmoji',
-  'messageUpdate',
-  // 'rateLimit', // TODO: figure out how to handle this
-  'roleCreate',
-  'roleDelete',
-  'roleUpdate',
-  'shardDisconnect',
-  'shardError',
-  'shardReconnecting',
-  'shardResume',
-  'userUpdate',
-  'warn',
-  'webhookUpdate',
-  'voiceStateUpdate',
+// TODO: warn for new accounts
+const memberEventNames = ['guildMemberAdd', 'guildMemberRemove']
+// TODO: Figure out how to handle attachments?
+const messageEventNames = ['messageDelete', 'messageDeleteBulk', 'messageUpdate']
+
+const loggingChannels = [MOD_LOG_CHANNEL_ID, PUBLIC_LOG_CHANNEL_ID, ADMIN_LOG_CHANNEL_ID]
+const administratorChannels = [
+  '764415739423096832', // #council-backups
+  '761323976303050802', // #council-chat
+  '763170044984688640', // #cuties-chat
+  '763182857555673089', // #cuties-nsfw
 ]
 
-// TODO: these are events crate far too much spam and are not useful except for debugging. Maybe keep track of them somewhere else? or a spam channel?
-// 'typingStart'
-// 'shardReady', TODO: handle this separate, more debugging info
-// 'presenceUpdate',
-// 'message',  TODO: figure out how to exclude channels
-// 'debug',
-// 'ready',
-
-// TODO: may need black list by event..... could be tricky with replacer... maybe need to bind eventName to its
-// TODO: do I need message TYPE? what are the types
-// TODO: blacklist url for invites since it pops up in the logs, and id is enough
-const blackListedProperties = [
+const blackListedPrintProperties = [
   'guildID', // messages posted in the guild specific log already
   'defaultAvatarURL', // this is the same for all users and does not matter for logging purposes
   'discriminator', // tag and user id make this clear, separate field is unnecessary
@@ -70,64 +27,78 @@ const blackListedProperties = [
   'bot', // almost all events will be not bot commands, and userId is enough
   'lastMessageChannelID', // never necessary since rest of event contains all info
   'avatarURL', // displayAvatarURL is good enough
-
-  // friends guild specific config
-  'tts', // tts disabled in friends guild
+  'deleted', // Not needed because the event indicates if the message is deleted,
+  'tts', // tts is often turned off and not valuable for moderation
+  'pinned', // pinned status is tracked by audit log and not valuable for moderation
+  'type', // Not important for the events tracked by this bot https://discord.com/developers/docs/resources/channel#message-object-message-types
+  'system', // not important for moderation and will not happen given the events tracked
 ]
+const blacklistedZeroPrintProperties = ['flags', 'editedTimestamp', 'createdTimestamp']
 
-const blackListedChannels = [
-  '765005154377400350', // emmabot -> log
-]
-
-// TODO: convert timestamps to to human readable formats
-// TODO: hide empty properties
-// TODO: make flags human readable? or just link to docs?
-// TODO: show emoji in discord
-function replacer(name, val) {
-  if (blackListedProperties.includes(name)) {
+// TODO: look up cahnnel and
+function jsonStringifyReplacer(name, val) {
+  if (blackListedPrintProperties.includes(name)) {
     return undefined
   }
 
-  // hide empty properties that aren't expl
-  if (!val && val !== false) {
+  if (val === undefined || val == null) {
     return undefined
+  }
+
+  if (isObjectLike(val) && isEmpty(val)) {
+    return undefined
+  }
+
+  if (blacklistedZeroPrintProperties.includes(name) && val === 0) {
+    return undefined
+  }
+
+  if (name.toLowerCase().endsWith('timestamp')) {
+    return new Date(val).toTimeString()
   }
 
   return val
 }
 
-export default function setupEventHandlersForLogging(): void {
-  for (const eventHandlerName of eventHandlerNames) {
-    DiscordClient.on(eventHandlerName, async (...args: any[]) => {
-      // TODO: ensure all events have channelId
-      for (const arg in args) {
-        if (arg && blackListedChannels.includes(arg['channelId'])) {
-          return
+function printEvent(eventName: string, eventArgs: any[]): string {
+  return eventName + '\n```\n' + JSON.stringify(eventArgs, jsonStringifyReplacer, 2) + '\n```'
+}
+
+function setupEventHandlers(
+  eventNames: string[],
+  loggingChannelId: string,
+  channelBlacklist: string[] | null,
+  channelWhitelist: string[] | null,
+) {
+  for (const eventName of eventNames) {
+    if (channelBlacklist && channelWhitelist) {
+      throw new Error('Cannot define whitelist and blacklist')
+    }
+
+    DiscordClient.on(eventName, async (...args: any[]) => {
+      if (!channelBlacklist && !channelWhitelist) {
+        for (const arg in args) {
+          const channelId = arg['channelId']
+
+          if (channelBlacklist && channelBlacklist.includes(channelId)) {
+            return
+          }
+
+          if (channelWhitelist && !channelWhitelist.includes(channelId)) {
+            return
+          }
         }
       }
 
-      // Note ignore all voice state updates that are not join and leave
-      // TODO: log when sver mut, deafens, or admin moves happen
-      if (eventHandlerName == 'voiceStateUpdate') {
-        const before = args[0]
-        const after = args[1]
-
-        if (before['channel'] == after['channel']) {
-          return
-        }
-      }
-
-      // TODO: get guild id from args instead of using only friends guild id (ensure all events have guildId)
-      // TODO: separate logging for council specific channels like cuties and council-chat
-      // TODO: separate message logs into a different channel for homies and above
-      // TODO: log full events somewhere else like S3 or database...consider commented out events as well
-      // TODO: consider attaching images and attachments to each message?? worry about size limits...
-      // TODO: consider creating mapping of event to argument name??
-      await sendMessage(
-        FRIENDS_GUILD_ID,
-        LOG_CHANNEL_ID,
-        eventHandlerName + '\n```\n' + JSON.stringify(args, replacer, 2) + '\n```',
-      )
+      console.log(loggingChannelId)
+      await sendMessage(FRIENDS_GUILD_ID, loggingChannelId, printEvent(eventName, args))
     })
   }
+}
+
+// Message schema - https://discord.com/developers/docs/resources/channel#message-object
+export default function setupEventHandlersForLogging(): void {
+  setupEventHandlers(memberEventNames, PUBLIC_LOG_CHANNEL_ID, null, null)
+  setupEventHandlers(messageEventNames, MOD_LOG_CHANNEL_ID, loggingChannels.concat(administratorChannels), null)
+  setupEventHandlers(messageEventNames, ADMIN_LOG_CHANNEL_ID, null, administratorChannels)
 }
