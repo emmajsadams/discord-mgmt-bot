ddbe18077842760e0455b391e387f0a63557bc1b
Emma Cline <7471922-EmmaJCline@users.noreply.gitlab.com>
2020-10-13 14:31:06 -0700

    Separate discordClient creation into function

--- src/discordClient.ts
+++ src/discordClient.ts
@@ -1,18 +1,7 @@
-import { Client } from 'discord.js'
+import { EMMA_BOT } from './config/users'
+import getDiscordClient from './getDiscordClient'
 
-// TODO: Change status to be something clever about learning to become human
-// TODO: Use a separate call to update presence
-// TODO: client options and setup autoreconnect if I too https://discord.js.org/#/docs/main/stable/typedef/ClientOptions
-// NOTE: discord client manages automatically retrying and reconnecting.
-const DiscordClient = new Client({
-  presence: {
-    activity: { type: 'WATCHING', name: 'the matrix' },
-    status: 'online',
-    afk: false,
-  },
-  retryLimit: 3,
-})
+// TODO: Note this file is about the general bot.
+const EmmaBotClient = getDiscordClient(EMMA_BOT)
 
-DiscordClient.login(process.env.DISCORD_BOT_TOKEN)
-
-export default DiscordClient
+export default EmmaBotClient
