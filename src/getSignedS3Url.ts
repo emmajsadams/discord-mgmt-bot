26847d3b2a7543f9bd4af8b75af34d10d524c8d5
Emma Cline <7471922-EmmaJCline@users.noreply.gitlab.com>
2020-10-13 00:16:15 -0700

    Revoke expiration of signed urls

--- src/getSignedS3Url.ts
+++ src/getSignedS3Url.ts
@@ -1,9 +1,10 @@
 import { S3_BUCKET, S3_API } from './ids'
 
-export default async function getSignedS3Url(key) {
-  return await S3_API.getSignedUrl('getObject', {
+export default async function getSignedS3Url(key): Promise<string> {
+  // TODO: consider making link expire? files will be deleted so it doesn't matter
+  return S3_API.getSignedUrl('getObject', {
     Bucket: S3_BUCKET,
     Key: key,
-    Expires: 60 * 30, // TODO: revisit this expire time based on how I rotate backups
+    // Expires: 60 * 30, // TODO: revisit this expire time based on how I rotate backups
   })
 }
