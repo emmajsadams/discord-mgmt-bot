04db3dbcb463b26dda64a8a3074a88d345e98d11
Emma Cline <7471922-EmmaJCline@users.noreply.gitlab.com>
2020-10-15 22:27:36 -0700

    check for disabled flag in setupCron

--- src/setupCron.ts
+++ src/setupCron.ts
@@ -1,8 +1,13 @@
 import { schedule } from 'node-cron'
 import backup from './backup'
+import isDisabled from './isDisabled'
 
 export default function setupCron() {
   schedule('0 0 * * *', async () => {
+    if (isDisabled('backup')) {
+      return
+    }
+
     await backup()
   })
 }
