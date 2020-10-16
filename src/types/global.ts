45fc91a15630845954ab032d774b025440ff05e2
Emma Cline <7471922-EmmaJCline@users.noreply.gitlab.com>
2020-10-15 22:28:06 -0700

    move global types to types folder

---
+++ src/types/global.ts
@@ -0,0 +1,10 @@
+export {} // TODO: why does this need to be a modlule?? https://stackoverflow.com/questions/57132428/augmentations-for-the-global-scope-can-only-be-directly-nested-in-external-modul
+
+declare global {
+  /**
+   * Fix for axios while we wait for ^0.2.1 to be published
+   * merge: https://github.com/axios/axios/commit/b7e954eba3911874575ed241ec2ec38ff8af21bb
+   * issue: https://github.com/axios/axios/issues/3219
+   */
+  interface ProgressEvent {} // eslint-disable-line
+}
