f3f2585eadf5c304d7b7a2754a0ebcda29efab63
Emma Cline <7471922-EmmaJCline@users.noreply.gitlab.com>
2020-10-30 15:51:42 -0700

    Update roles config

---
+++ src/config/roles.ts
@@ -0,0 +1,4 @@
+// Role assigned to everyone when they join. If null no role is assigned and the default everyone is used
+export const EVERYONE_ROLE = '768252236425068576' // Civilian role
+
+// TODO: Add config option for autorole/autoroles?
