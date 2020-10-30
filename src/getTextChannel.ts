b407c785309505afa83171f9303b60bd0595ed69
Emma Cline <7471922-EmmaJCline@users.noreply.gitlab.com>
2020-10-30 15:33:11 -0700

    Add function to getTextChannel for BotConfig and ChannelId

---
+++ src/getTextChannel.ts
@@ -0,0 +1,14 @@
+import { Snowflake, TextChannel } from 'discord.js'
+import { BotConfig, PRIMARY_BOT } from './config/bots'
+import getGuild from './getGuild'
+
+// TODO: add support for development environment variable (prod/dev server)
+// TODO: do I want a generic getChannel or textchannel? Do I validate if textchannel or not
+export default async function getTextChannel(
+  channelId: Snowflake,
+  botConfig: BotConfig = PRIMARY_BOT,
+): Promise<TextChannel> {
+  const guild = await getGuild(botConfig)
+
+  return Promise.resolve(guild.channels.resolve(channelId) as TextChannel)
+}
