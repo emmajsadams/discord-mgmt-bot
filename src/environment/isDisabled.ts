c796fe0f8aaebad0151a9dc84f7ebf06545eebf0
Emma Cline <7471922-EmmaJCline@users.noreply.gitlab.com>
2020-10-30 15:31:01 -0700

    Add function to getDisableEnv environment variable

---
+++ src/environment/isDisabled.ts
@@ -0,0 +1,7 @@
+// TODO: change this to featureIsEnabled (I only ever check for that)
+// TODO: create enum instead of string
+export default function getDisableEnv(name: string): boolean {
+  const stringBoolean = process.env[`DISABLE_${name.toUpperCase()}`]
+
+  return stringBoolean === 'true'
+}
