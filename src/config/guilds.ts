107a8d04280c59b3a58323a69e7748adc293493d
Emma Cline <7471922-EmmaJCline@users.noreply.gitlab.com>
2020-10-30 15:49:41 -0700

    Update guilds config

---
+++ src/config/guilds.ts
@@ -0,0 +1,4 @@
+import { Snowflake } from 'discord.js'
+
+// Currently there will be support for secondary backup guilds and development guild. For now it's only the primary guild
+export const PRODUCTION_GUILD_ID: Snowflake = '767889405393895455'
