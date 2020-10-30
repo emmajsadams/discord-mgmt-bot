1d8f847702f6536b5a9c449be9f63af929c20bc0
Emma Cline <7471922-EmmaJCline@users.noreply.gitlab.com>
2020-10-30 15:50:07 -0700

    Add todo to change getTextChannel function to acept guild

--- src/getTextChannel.ts
+++ src/getTextChannel.ts
@@ -4,6 +4,7 @@ import getGuild from './getGuild'
 
 // TODO: add support for development environment variable (prod/dev server)
 // TODO: do I want a generic getChannel or textchannel? Do I validate if textchannel or not
+// TODO: accept a Guild object instead of botconfig!
 export default async function getTextChannel(
   channelId: Snowflake,
   botConfig: BotConfig = PRIMARY_BOT,
