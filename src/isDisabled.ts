598bfac2ba7832277aaf39223e2e80df34d07255
Emma Cline <7471922-EmmaJCline@users.noreply.gitlab.com>
2020-10-31 18:18:19 -0700

    Remove isDisabled env

--- src/isDisabled.ts
+++
@@ -1,6 +0,0 @@
-// TODO: validate boolean string
-export default function getDisableEnv(name: string): boolean {
-  const stringBoolean = process.env[`DISABLE_${name.toUpperCase()}`]
-
-  return stringBoolean === 'true'
-}
