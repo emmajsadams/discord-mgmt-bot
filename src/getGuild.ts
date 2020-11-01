7e50536e0c6f6c73fd5fa1d9e7ff933c45b5a641
Emma Cline <7471922-EmmaJCline@users.noreply.gitlab.com>
2020-10-31 17:05:04 -0700

    Add function to get a guild referened to a bot

---
+++ src/getGuild.ts
@@ -0,0 +1,13 @@
+import { Guild } from 'discord.js'
+import { BotConfig, PRIMARY_BOT } from './config/bots'
+import { PRODUCTION_GUILD_ID } from './config/guilds'
+import getDiscordClient from './getClient'
+
+// TODO: add support for development environment variable (prod/dev server)
+export default async function getGuild(
+  botConfig: BotConfig = PRIMARY_BOT,
+): Promise<Guild> {
+  const client = await getDiscordClient(botConfig)
+
+  return client.guilds.fetch(PRODUCTION_GUILD_ID)
+}
