bf7bec9aa5d5b4235a7f3b23417b1968c2fc38ae
Emma Cline <7471922-EmmaJCline@users.noreply.gitlab.com>
2020-10-31 18:22:57 -0700

    Replace discord client singleton with function

--- src/discordClient.ts
+++
@@ -1,7 +0,0 @@
-import { EMMA_BOT } from './config/users'
-import getDiscordClient from './getDiscordClient'
-
-// TODO: Note this file is about the general bot.
-const EmmaBotClient = getDiscordClient(EMMA_BOT)
-
-export default EmmaBotClient
---
+++ src/getPrimaryClient.ts
@@ -0,0 +1,7 @@
+import { Client } from 'discord.js'
+import { PRIMARY_BOT } from './config/bots.js'
+import getDiscordClient from './getClient.js'
+
+export default async function getPrimaryClient(): Promise<Client> {
+  return getDiscordClient(PRIMARY_BOT)
+}
