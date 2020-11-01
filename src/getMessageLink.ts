15ce0c3abd0f0ca96070b3c064821ea82d22c427
Emma Cline <7471922-EmmaJCline@users.noreply.gitlab.com>
2020-10-31 18:28:27 -0700

    Remove getMessageLink duplicate

--- src/getMessageLink.ts
+++
@@ -1,5 +0,0 @@
-import { Message } from 'discord.js'
-
-export default function getMessageLink(message: Message): string {
-  return `https://discord.com/channels/${message.guild.id}/${message.channel.id}/${message.id}`
-}
