1e4192324045b3dbf82600ef4f5c6554451e6eae
Emma Cline <7471922-EmmaJCline@users.noreply.gitlab.com>
2020-10-31 18:34:28 -0700

    Refactor setupPrimaryBot

--- src/setupPrimaryBot.ts
+++ src/setupPrimaryBot.ts
@@ -1,93 +1,72 @@
-import EmmaBotClient from './discordClient'
-import autorole from './autorole'
-import { FRIENDS_GUILD_ID } from './ids'
-import { MUSIC_BOT_CHANNEL } from './config/channels'
-
-import CACHED_MESSAGES from './cachedMessages'
-import { Client } from 'discord.js'
-
-import filterMessage from './filterMessage'
-import updateReactionRole from './updateReactionRole'
-import { EMMA_BOT, SECONDARY_BOTS } from './config/users'
-import getDiscordClient from './getDiscordClient'
-import setupMusicBots from './setupMusicBots'
-import replyToBotMention from './replyToBotMention'
-import setupLogging from './setupLogging'
-import createMusicPlayers from './createMusicPlayers'
-import pify from 'pify'
-import isDisabled from './isDisabled'
+import { GuildMember } from 'discord.js'
+import autorole from './autorole.js'
+import { PRIMARY_BOT } from './config/bots.js'
+import { MUSIC_QUEUE_CHANNEL_ID } from './config/channels.js'
+import { EVERYONE_ROLE } from './config/roles.js'
+import isDisabled from './environment/isDisabled.js'
+import filterMessageFromConfig from './filterMessageFromConfig.js'
+import getPrimaryClient from './getPrimaryClient.js'
+import setupLogging from './logging/setupLogging.js'
+import createMusicPlayers from './music/createMusicPlayers.js'
+import setupMusicBots from './music/setupMusicBots.js'
+import updateReactionRole from './reactionRoles/updateReactionRole.js'
+import replyToBotMention from './reply/replyToBotMention.js'
 
 // TODO: change this to be something about setup all bots
-export default async function setupPrimaryBot() {
-  // Setup secondary bots
-  const secondaryClients: Client[] = []
-  const onReadyPromises: Promise<void>[] = [
-    pify(EmmaBotClient.on.bind(EmmaBotClient))('ready'),
-  ]
-  for (const botConfig of SECONDARY_BOTS) {
-    const discordClient = getDiscordClient(botConfig)
-    discordClient.on('ready', () => {
-      discordClient.user.setPresence(botConfig.presence)
+export default async function setupPrimaryBot(): Promise<void> {
+  const primaryClient = await getPrimaryClient()
+
+  // Reaction roles
+  if (!isDisabled('reaction_roles')) {
+    primaryClient.on('messageReactionAdd', async (messageReaction, user) => {
+      await updateReactionRole(messageReaction, user, true)
     })
-    secondaryClients.push(discordClient)
+    primaryClient.on('messageReactionRemove', async (messageReaction, user) => {
+      await updateReactionRole(messageReaction, user, false)
+    })
   }
 
-  for (const secondaryClient of secondaryClients) {
-    onReadyPromises.push(
-      pify(secondaryClient.on.bind(secondaryClient))('ready'),
-    )
+  // Autorole
+  if (!isDisabled('autorole')) {
+    primaryClient.on('guildMemberAdd', async (member) => {
+      await autorole(member as GuildMember, EVERYONE_ROLE)
+    })
   }
 
-  await Promise.all(onReadyPromises)
-
-  const musicBotMessages = await createMusicPlayers(
-    secondaryClients,
-    FRIENDS_GUILD_ID,
-    MUSIC_BOT_CHANNEL,
-  )
-
-  // Create copy of secondary bots array that can be modified and treated as a stack
-  const availableMusicClients = secondaryClients.map((client) => client)
-
-  console.log(`Logged in as ${EmmaBotClient.user.tag}!`)
-  EmmaBotClient.user.setPresence(EMMA_BOT.presence)
-  const guild = await EmmaBotClient.guilds.fetch(FRIENDS_GUILD_ID)
-
-  // Cache all messages
-  for (const channelId in CACHED_MESSAGES) {
-    const channel = await guild.channels.cache.get(channelId)
-    // @ts-expect-error ts-migrate(2339) FIXME: Property 'messages' does not exist on type 'GuildC... Remove this comment to see the full error message
-    await channel.messages.fetch(CACHED_MESSAGES[channelId])
+  // TODO: type this
+  let musicBotMessages
+  if (!isDisabled('music')) {
+    musicBotMessages = await createMusicPlayers(MUSIC_QUEUE_CHANNEL_ID)
   }
 
-  EmmaBotClient.on('messageReactionAdd', async (messageReaction, user) => {
-    await updateReactionRole(messageReaction, user, true)
-  })
-
-  EmmaBotClient.on('messageReactionRemove', async (messageReaction, user) => {
-    await updateReactionRole(messageReaction, user, false)
-  })
-
-  EmmaBotClient.on('guildMemberAdd', async (member) => {
-    await autorole(member)
-  })
-
-  EmmaBotClient.on('message', async (message) => {
-    const filterReason = filterMessage(message.content)
+  primaryClient.on('message', async (message) => {
+    const filterReason = filterMessageFromConfig(message)
     if (filterReason) {
       message.delete({ reason: filterReason })
       message.reply(filterReason)
+      return
     }
 
     if (
       !isDisabled('music') &&
-      (await setupMusicBots(musicBotMessages, availableMusicClients, message))
+      (await setupMusicBots(musicBotMessages, message))
     ) {
       return
     }
+
+    if (
+      !isDisabled('replies') &&
+      (await replyToBotMention(PRIMARY_BOT, message))
+    ) {
+      return
+    }
+
+    console.info(`message ${message.id} was not actionable`)
   })
 
   if (!isDisabled('logging')) {
     setupLogging()
   }
+
+  return Promise.resolve()
 }
